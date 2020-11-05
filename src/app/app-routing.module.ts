47b9fb885c5276ec2fa7103740028272f9b74cab
ShweSinHtay <shwesin@isgm2.com>
2020-11-05 17:58:51 +0630

    nn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'employee', pathMatch: 'full' },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'add', component: CreateEmployeeComponent },
-  { path: 'update/:id', component: CreateEmployeeComponent },
+  { path: 'update/:id', component: UpdateEmployeeComponent },
   { path: 'details/:id', component: EmployeeDetailsComponent },
 ];
 
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,20 +1,20 @@
 import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
-import { Observable } from "rxjs";
-import { EmployeeService } from "../employee.service";
-import { Employee } from "../employee";
-import { Component, OnInit } from "@angular/core";
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-employee-list",
-  templateUrl: "./employee-list.component.html",
-  styleUrls: ["./employee-list.component.css"]
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
   employees: Observable<Employee[]>;
 
   constructor(private employeeService: EmployeeService,
-    private router: Router) {}
+    private router: Router) { }
 
   ngOnInit() {
     this.reloadData();
@@ -34,7 +34,10 @@ export class EmployeeListComponent implements OnInit {
         error => console.log(error));
   }
 
-  employeeDetails(id: number){
+  employeeDetails(id: number) {
     this.router.navigate(['details', id]);
+  }
+  updateEmployee(id: number) {
+    this.router.navigate(['update', id]);
   }
 }
